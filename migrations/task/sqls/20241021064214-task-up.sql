a8fecc53d23b890039e9a29f73777be7f5ebaef6
jelly <jelly.jiang@mwg.com.tw>
2024-12-08 10:55:56 +0800

    update 5-8

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -309,6 +309,31 @@ group by "COURSE_BOOKING".user_id
     -- inner join ( 用戶王小明的已使用堂數) as "COURSE_BOOKING"
     -- on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
+SELECT 
+    "CREDIT_PURCHASE".user_id,
+    (
+    "CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit
+    ) AS remaining_credit
+FROM 
+    (SELECT 
+        user_id, 
+        SUM(purchased_credits) AS total_credit
+     FROM "USER"
+     INNER JOIN "CREDIT_PURCHASE" ON "USER".id = "CREDIT_PURCHASE".user_id
+     WHERE email = 'wXlTq@hexschooltest.io'
+     GROUP BY user_id
+    ) AS "CREDIT_PURCHASE"
+INNER JOIN 
+    (SELECT 
+        user_id,
+        COUNT(*) AS used_credit
+     FROM "USER"
+     INNER JOIN "COURSE_BOOKING" ON "USER".id = "COURSE_BOOKING".user_id
+     WHERE
+      "USER".email = 'wXlTq@hexschooltest.io'
+      AND "COURSE_BOOKING".cancelled_at IS NULL
+     GROUP BY user_id
+    ) AS "COURSE_BOOKING" ON "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
 -- ████████  █████   █     ███
 --   █ █   ██    █  █     █
